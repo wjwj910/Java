90594bfb14a83c6e94bffe71df16a0f6e327700a
wjwj910 <wjwj910@gmail.com>
2022-03-24 00:06:05 +0900

    test용으로 수정함

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for(int i=0;i<=10;i++) {
 			System.out.println("Hello World!");
+		}
+			
 	}
 
 }
